bfeb0ec71f3c9fd3deedc596cf6435430c581757
corbie0091 <corbie0091@gmail.com>
2024-11-01 17:08:58 +0900

    rebase 이후 수정

--- src/R051.js
+++ src/R051.js
@@ -8,3 +8,17 @@
 // fast-forward merge try
 
 // rebase
+
+// Case3. git rebase & merge
+// rebase 쓰는 이유: 3-way merge는 나중에 git log 출력에 복잡해보임
+// 즉 간단하고 짧은 브랜치들은 이거 쓰면 깔끔해보일 수 있음
+
+// 단점: conflict엔딩이 많이 날 수 있다.
+
+// 일반 merge는 1. 중심브랜치로 이동 , git merge 새로운 브랜치명을 만들어줘야함
+
+// rebase & merge하고싶으면
+// 1. 새로운 브랜치로 이동
+// 2.git rebase중심브랜치명
+// 3. 중심브랜치 이동해서
+// 4. git merge 새로운 브랜치명
